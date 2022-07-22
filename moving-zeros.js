a46c2fae8855676e04a30a56b8a33e4509abe526
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-07-22 14:03:25 -0400

    need to come back

---
+++ moving-zeros.js
@@ -0,0 +1,20 @@
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+function moveZeros(arr) {
+    //console.log(arr.length)
+    str=arr.join('')
+    
+    //console.log(str.length)
+    while(arr.includes('0')){
+    arr=arr.replace('0','')
+    
+}str=arr.join('')
+rep=str
+    split=rep.split('')
+    
+     for (let i=0;i<split.length;i++){
+        if (Number.isInteger(Number(split[i])))
+        split[i]=Number(split[i])
+     }
+      return split ;
+    }
+console.log(moveZeros([false, 1, 2, 1, 0, 1, 0, 3, 0, 1, 0 ]));
